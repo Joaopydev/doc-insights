27b88372185e7ec90c00bbaffacb484289bc1664
Joao <joao.ribeiro@e-deploy.com.br>
2026-07-05 11:36:31 -0300

    minor changes

--- src/identity/application/use_cases/signup/user_signup_dto.py
+++ src/identity/application/use_cases/signup/user_signup_dto.py
@@ -1,10 +1,11 @@
-from pydantic import BaseModel, EmailStr
+from typing import Annotated
+from pydantic import BaseModel, EmailStr, Field
 
 
 class SignupInput(BaseModel):
     email: EmailStr
-    name: str
-    password: str
+    name: Annotated[str, Field(..., min_length=3, max_length=100)]
+    password: Annotated[str, Field(..., min_length=8, max_length=100)]
 
 class SignupOutput(BaseModel):
     access_token: str
