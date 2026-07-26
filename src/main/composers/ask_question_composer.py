ab9454221dc7a0283a8498b1400e5a5aae8c5483
Joao <joao.ribeiro@e-deploy.com.br>
2026-07-26 10:39:26 -0300

    implement ask question lambda function

---
+++ src/main/composers/ask_question_composer.py
@@ -0,0 +1,24 @@
+from typing import Callable
+
+from src.chat.presentation.controllers.question_controller import QuestionController
+from src.chat.application.use_cases.ask_question import AskQuestionUseCase
+from src.chat.infrastructure.chat_repository import ChatRepository
+
+from src.shared.infrastructure.eventbridge.client import EventBridgeClient
+from src.shared.infrastructure.dynamodb.client import DynamoDBClient
+
+
+class AskQuestionComposer:
+
+    @staticmethod
+    def compose() -> Callable:
+
+        repo = ChatRepository(DynamoDBClient())
+        event_publisher = EventBridgeClient()
+        use_case = AskQuestionUseCase(
+            chat_repository=repo,
+            event_publisher=event_publisher
+        )
+        controller = QuestionController(use_case)
+
+        return controller.handle
