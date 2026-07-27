46a77fba4c5ad07190ccd835aa273452b19b5d35
Joao <joao.ribeiro@e-deploy.com.br>
2026-07-27 16:03:22 -0300

    add try except to test quesiton processing lambda function

--- src/functions/question_processing/handler.py
+++ src/functions/question_processing/handler.py
@@ -1,13 +1,17 @@
 import asyncio
 from typing import Dict, Any
+import traceback
 
 from src.chat.application.events.question_asked_event import QuestionAskedEvent
 from src.main.composers.question_processing_composer import QuestionProcessingComposer
 
 
 async def async_handler(event: Dict[str, Any], context: Any):
-    compose = QuestionProcessingComposer.compose()
-    await compose(QuestionAskedEvent(event["detail"]["message_id"]))
+    try:
+        compose = QuestionProcessingComposer.compose()
+        await compose(QuestionAskedEvent(event["detail"]["message_id"]))
+    except Exception:
+        traceback.print_exc()
 
 def handler(event: Dict[str, Any], context: Any):
     asyncio.run(async_handler(event, context))
