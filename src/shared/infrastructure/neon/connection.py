d118236564374a42c6c74659d5c4a42139a01dd7
Joao <joao.ribeiro@e-deploy.com.br>
2026-07-27 17:49:22 -0300

    add pgvector driver

--- src/shared/infrastructure/neon/connection.py
+++ src/shared/infrastructure/neon/connection.py
@@ -1,6 +1,7 @@
 # pylint: disable=no-member
 from typing import Optional
 from psycopg import connect, Connection
+from pgvector.psycopg import register_vector
 
 from src.main.config.settings import settings
 
@@ -12,6 +13,7 @@ class VectorDatabaseConnection:
 
     def __enter__(self) -> Connection:
         self.connection = connect(settings.neon_database_url)
+        register_vector(self.connection)
         return self.connection
 
     def __exit__(self, exc_type, exc_val, exc_tb) -> None:
